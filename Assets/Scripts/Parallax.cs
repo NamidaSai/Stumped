d7d63e23af3d3adcbf7f1218c30496b498e4d552
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-06-13 08:36:28 +0100

    Parallax Set Up On Camera Prefab

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour 
+public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    public GameObject cam;
-    public float parallaxEffect;
+	[SerializeField] bool followsVertical = false;
+	private float length, startpos, startYPos;
+	public GameObject cam;
+	public float parallaxEffect;
 
-    void Start()
-    {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
+	void Start()
+	{
+		startpos = transform.position.x;
+		startYPos = transform.position.y;
+		length = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
 
-    void Update()
-    {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+	void Update()
+	{
+		float temp = (cam.transform.position.x * (1 - parallaxEffect));
+		float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+		if (followsVertical)
+		{
+			transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+		}
+		else
+		{
+			transform.position = new Vector3(startpos + dist, startYPos, transform.position.z);
+		}
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
-    }
+		if (temp > startpos + length) startpos += length;
+		else if (temp < startpos - length) startpos -= length;
+	}
 }
