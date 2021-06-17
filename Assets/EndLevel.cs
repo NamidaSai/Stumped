95e9406ee0afb7c4958d004bcd6738ae1fc55053
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2021-06-17 12:04:26 +0100

    Set up End Level Animations

--- Assets/EndLevel.cs
+++ Assets/EndLevel.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class EndLevel : MonoBehaviour
 {
+	[SerializeField] Transform holeTransform = default;
+	[SerializeField] float itemSuckDistance = 0.3f;
+	[SerializeField] float suckedInSpeed = 0.65f;
+	private GameObject winItem = null;
+	private bool levelHasEnded = false;
+
+	private void Update()
+	{
+		if (winItem != null && levelHasEnded)
+		{
+			BringItemToHole();
+		}
+	}
+
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "WinItem")
+		{
+			winItem = other.gameObject;
+			other.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+			other.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			other.enabled = false;
+		}
+
+		if (other.gameObject.tag == "Player" && winItem != null)
 		{
 			TriggerEndLevel();
 		}
 	}
 
+	private void BringItemToHole()
+	{
+		float distanceToItem = Vector2.Distance(winItem.transform.position, holeTransform.position);
+
+		if (distanceToItem > itemSuckDistance)
+		{
+			winItem.transform.position = Vector2.MoveTowards(winItem.transform.position, holeTransform.position, Time.deltaTime * suckedInSpeed);
+		}
+		else
+		{
+			winItem.GetComponent<Animator>().SetTrigger("SuckedIn");
+			Destroy(winItem, 1f);
+		}
+	}
+
 	private void TriggerEndLevel()
 	{
-		Debug.Log("Level ended!");
-		FindObjectOfType<PlayerController>().GetComponentInChildren<Animator>().SetTrigger("Success");
+		levelHasEnded = true;
+		FindObjectOfType<PlayerController>().GetComponentInChildren<Animator>().SetBool("Success", true);
 		FindObjectOfType<PlayerController>().GetComponent<PlayerController>().enabled = false;
 	}
 }
